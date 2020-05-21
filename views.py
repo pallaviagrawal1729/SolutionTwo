2d35ba4ef7b3abeddd43f305ce2aa576a93506b1
Pallavi Agrawal <wacky.pallavi@gmail.com>
2020-05-21 15:55:36 +0530

    Third Version

--- views.py
+++ views.py
@@ -6,19 +6,20 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = b'\x9e\x8d\x9a\x91\ts\x0b\xa8\x17\x01\xffe\\\xc4\xf1\xb7\xec#\xcf\x96\x14\xacs\x9f'
 
-@app.route("/",methods=['GET', 'POST'])
+
+@app.route("/", methods=['GET', 'POST'])
 def welcome():
     form = Information()
     if form.validate_on_submit():
-        # tests = form.tests.data
-        # phoneModel = form.phoneModel.data
-        # phoneOS = form.phoneOS.data
-        # device = form.device.data
-        # FirmwareVersion = form.firmwareVersion.data
-        return redirect(url_for('report'))
+        tests = form.tests.data
+        phoneModel = form.phoneModel.data
+        phoneOS = form.phoneOS.data
+        device = form.device.data
+        FirmwareVersion = form.firmwareVersion.data
+        return render_template("generateTable.html",form=form)
     return render_template("firstPage.html", form=form)
 
 
-@app.route("/testReport", methods=['GET'])
-def report():
-    return render_template("generateTable.html")
+# @app.route("/testReport", methods=['GET'])
+# def report(form):
+#     return render_template("generateTable.html", form=form)
