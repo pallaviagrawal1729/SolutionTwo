f0d27cc983c45169c34c483c90b6395c698a802b
Pallavi Agrawal <wacky.pallavi@gmail.com>
2020-05-23 10:40:22 +0530

    Updated views

--- views.py
+++ views.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = b'\x9e\x8d\x9a\x91\ts\x0b\xa8\x17\x01\xffe\\\xc4\xf1\xb7\xec#\xcf\x96\x14\xacs\x9f'
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/welcome", methods=['GET', 'POST'])
 def welcome():
     form = Information()
     if form.validate_on_submit():
